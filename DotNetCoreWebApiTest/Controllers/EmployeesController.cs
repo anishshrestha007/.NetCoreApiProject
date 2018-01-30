00fb0ad66d5875de7bf88ab77c9b069227f7fd16
anish.shrestha <anishronnie@gmail.com>
2018-01-30 14:09:01 +0545

    First commit

---
+++ DotNetCoreWebApiTest/Controllers/EmployeesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreWebApiTest.ModelContext;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreWebApiTest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Employees")]
+    [AllowAnonymous]
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Employees
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _context.employees;
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.employees.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+        
+        // POST: api/Employees
+       
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodo", new { id = employee.Id }, employee);
+        }
+        
+        // PUT: api/Employees/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.employees.SingleOrDefaultAsync(m => m.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.employees.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+        private bool EmployeeExists(int id)
+        {
+            return _context.employees.Any(e => e.Id == id);
+        }
+    }
+}
